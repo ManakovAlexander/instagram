01a73a953fc8f1dd1989d24c4d663020fe1d3b00
Manakov Alexander <alexander.s.manakov@gmail.com>
2018-08-12 22:44:43 +0700

    Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,18 @@ import * as React from 'react';
 import './App.css';
 import AddPost from './containers/AddPost';
 import Posts from './containers/Posts';
+import { Link, Route } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Posts />
-        <AddPost />
+        <nav>
+          <Link to="/posts">Posts</Link>
+          <Link to="/add-post">Add post</Link>
+        </nav>
+        <Route path="/posts" component={Posts} />
+        <Route path="/add-post" component={AddPost} />
       </div>
     );
   }
