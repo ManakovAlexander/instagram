eba01377aaaf3c59f379420688b3dda6425cd926
Manakov Alexander <alexander.s.manakov@gmail.com>
2018-08-16 11:23:25 +0700

    Material UI

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { IPost } from 'src/models/post';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 interface IProps {
   post: IPost;
@@ -9,11 +11,13 @@ class Post extends React.Component<IProps> {
   render() {
     const post = this.props.post;
     return (
-      <div>
-        <img src={`http://localhost:1337/files/${post.photoId}`} width="300" />
-        <p>{post.title}</p>
-        { post.description && <p>{post.description}</p> }
-      </div>
+      <Card style={{ margin: 10 }}>
+        <CardContent>
+          <img src={`http://localhost:1337/files/${post.photoId}`} width="300" />
+          <p>{post.title}</p>
+          {post.description && <p>{post.description}</p>}
+        </CardContent>
+      </Card>
     );
   }
 }
