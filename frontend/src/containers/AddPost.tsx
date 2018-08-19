da87827e3a4d1fef690a5f8db1b826ec0ffc80ce
Manakov Alexander <alexander.s.manakov@gmail.com>
2018-08-19 17:02:50 +0700

    Add post: redesign

--- frontend/src/containers/AddPost.tsx
+++ frontend/src/containers/AddPost.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import Post from '../components/Post';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import { IPost } from 'src/models/post';
 
 interface IProps { }
@@ -7,8 +8,8 @@ interface IProps { }
 interface IState {
   file: File | null;
   imagePreviewUrl: string | null;
-  title: string | null;
-  description: string | null;
+  title: string | undefined;
+  description: string | undefined;
   post: IPost | null;
 }
 
@@ -16,8 +17,8 @@ class AddPost extends React.Component<IProps, IState> {
   readonly state: IState = {
     file: null,
     imagePreviewUrl: null,
-    title: null,
-    description: null,
+    title: '',
+    description: '',
     post: null
   };
   constructor(props: IProps) {
@@ -27,25 +28,51 @@ class AddPost extends React.Component<IProps, IState> {
   render() {
     return (
       <div>
-        <div>
-          Add photo
-          <form>
-            <input type="file" onChange={this.handleFileChange} />
-            { this.state.imagePreviewUrl && <img src={this.state.imagePreviewUrl} width="300" /> }
-            <input type="text" onChange={this.handleTitleChange} placeholder="title" required={true} />
-            <input type="text" onChange={this.handleDescriptionChange} placeholder="description" />
-          </form>
-          <button disabled={!this.postIsValid} onClick={this.sendPost}>Save</button>
-        </div>
-        {
-          this.state.post && <Post post={this.state.post} />
-        }
+        <form style={{ display: 'flex', flexDirection: 'column', margin: 8 }}>
+          <input
+            accept="image/*"
+            style={{ display: 'none' }}
+            id="file-button"
+            type="file"
+            onChange={this.handleFileChange}
+          />
+          <label htmlFor="file-button">
+            <Button variant="outlined" component="span">
+              Upload
+              </Button>
+          </label>
+          {this.state.imagePreviewUrl && <img src={this.state.imagePreviewUrl} width="300" />}
+          <TextField
+            id="title"
+            label="Title"
+            value={this.state.title}
+            onChange={this.handleTitleChange}
+            required={true}
+            style={{ margin: 8 }}
+          />
+          <TextField
+            id="description"
+            label="Description"
+            value={this.state.description}
+            onChange={this.handleDescriptionChange}
+            multiline={true}
+            style={{ margin: 8 }}
+          />
+          <Button
+            variant="outlined"
+            color="primary"
+            disabled={!this.postIsValid}
+            onClick={this.sendPost}
+            style={{ margin: 8 }}>
+            Save
+          </Button>
+        </form>
       </div>
     );
   }
 
-  handleFileChange = (ev: React.FormEvent<HTMLInputElement>) => {
-    if (!ev.currentTarget.files|| !ev.currentTarget.files[0]) {
+  handleFileChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    if (!ev.currentTarget.files || !ev.currentTarget.files[0]) {
       return;
     }
     const file = ev.currentTarget.files[0];
@@ -55,12 +82,12 @@ class AddPost extends React.Component<IProps, IState> {
     reader.readAsDataURL(file);
   }
 
-  handleTitleChange = (ev: React.FormEvent<HTMLInputElement>): void => {
-    this.setState({ title: ev.currentTarget.value });
+  handleTitleChange = (ev: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ title: ev.target.value });
   }
 
-  handleDescriptionChange = (ev: React.FormEvent<HTMLInputElement>): void => {
-    this.setState({ description: ev.currentTarget.value });
+  handleDescriptionChange = (ev: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ description: ev.target.value });
   }
 
   sendPost = () => {
