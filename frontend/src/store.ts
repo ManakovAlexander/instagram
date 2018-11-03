ce63fc4e8c79bb74404bf4ef21f3c92c29422c9a
Manakov Alexander <alexander.s.manakov@gmail.com>
2018-11-03 13:58:21 +0700

    Persisted state

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -3,14 +3,22 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import reducer from './reducers';
+import { loadState, saveState } from './localStorage';
 
 const composeEnhancers = composeWithDevTools({});
 
+const persistedState = loadState();
+
 const store = createStore(
   reducer,
+  persistedState,
   composeEnhancers(
     applyMiddleware(thunk)
   )
 );
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export default store;
