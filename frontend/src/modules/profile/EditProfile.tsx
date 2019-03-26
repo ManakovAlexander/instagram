f2c04bc86c688351a679dac6fede0660857c73d4
Manakov Alexander <alexander.s.manakov@gmail.com>
2019-03-26 12:05:51 +0300

    Frontend: Profile hooks

--- frontend/src/modules/profile/EditProfile.tsx
+++ frontend/src/modules/profile/EditProfile.tsx
@@ -1,44 +1,39 @@
-import * as React from "react";
-import { ICurrentUser } from "../../models/users";
-import { TextField } from "@material-ui/core";
+import React, { useState, FunctionComponent } from 'react';
+import isEqual from 'lodash/isEqual';
+import { TextField } from '@material-ui/core';
 
-import styles from "./EditProfile.module.css";
+import { ICurrentUser } from '../../models/users';
+
+import styles from './EditProfile.module.css';
 
 interface IProps {
   profile: ICurrentUser;
 }
 
-class State {
-  readonly profile: ICurrentUser | null = null;
-}
+const EditProfile: FunctionComponent<IProps> = (props: { profile: ICurrentUser }) => {
+  const [profile, setProfile] = useState<ICurrentUser | null>(null);
 
-export default class EditProfile extends React.PureComponent<IProps, State> {
-  readonly state = new State();
-
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      profile: this.props.profile
-    };
+  const shouldUpdateProfile = isEqual(props.profile, profile) === false;
+  if (shouldUpdateProfile) {
+    setProfile(props.profile);
   }
 
-  handleNameChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
-    if (this.state.profile) {
+  function handleNameChange(ev: React.ChangeEvent<HTMLInputElement>) {
+    if (profile) {
       const name = ev.currentTarget.value;
-      const profile = { ...this.state.profile, name };
-      this.setState({ profile });
+      setProfile({ ...profile, name });
     }
   }
 
-  render() {
-    const { profile } = this.state;
-    if (!profile) {
-      return null;
-    }
-    return (
-      <form noValidate={true} autoComplete="off" className={styles.form}>
-        <TextField id="standard-name" label="Name" value={profile.name} onChange={this.handleNameChange} />
-      </form>
-    );
+  if (!profile) {
+    return null;
   }
-}
+
+  return (
+    <form noValidate={true} autoComplete="off" className={styles.form}>
+      <TextField id="standard-name" label="Name" value={profile.name} onChange={handleNameChange} />
+    </form>
+  );
+};
+
+export default EditProfile;
